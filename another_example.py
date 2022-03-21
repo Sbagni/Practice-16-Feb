9258917f24e56c80ddac384fbc1074b66bbc6253
Gaurav Agnihotri <sumatibhala@gmail.com>
2022-03-21 10:34:17 -0400

    adding files

---
+++ another_example.py
@@ -0,0 +1,8 @@
+# Write a Python program to create the multiplication table (from 1 to 10) of a number.
+
+n = int(input("Input a number: "))
+
+# use for loop to iterate 10 times
+for i in range(1,11):
+   print(n,'x',i,'=',n*i)
+
